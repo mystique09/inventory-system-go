30a078b2c7583975496d9b0d3d831d664756daf0
mystique09 <knightkrypto7@gmail.com>
2022-02-09 12:36:15 +0800

    add user model

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
--- pkg/models/user.model.go
+++ pkg/models/user.model.go
@@ -1,1 +1,27 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Username string
+	Password string
+	Email    string
+	Role     string
+}
+
+type CreateUserDto struct {
+	Username string
+	Password string
+	Email    string
+	Role     string
+}
+
+type UpdateUserDto struct {
+	Username string
+	Password string
+	Email    string
+	Role     string
+}
