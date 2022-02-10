fca51bbb00d7de9ae95ca58220509ea7ff9a2b8e
mystique09 <knightkrypto7@gmail.com>
2022-02-10 21:19:05 +0800

    initialize db connection and automigrate models

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+
+	"inventory-system/pkg/db"
+	"inventory-system/pkg/models"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,6 +12,9 @@ import (
 )
 
 func main() {
+	conn := db.SetupConn()
+	conn.AutoMigrate(models.User{}, models.Product{})
+
 	app := echo.New()
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
@@ -15,5 +22,13 @@ func main() {
 	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, world")
 	})
+
+	user := app.Group("/user", middleware.JWT(func(key string) {
+		fmt.Println(key)
+	}))
+	user.GET("/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello!")
+	})
+
 	app.Logger.Fatal(app.Start(":8000"))
 }
