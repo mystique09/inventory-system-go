51a3d035e92c989c081c0245c52a45e5482509b7
mystique09 <knightkrypto7@gmail.com>
2022-02-10 14:42:54 +0800

    add user model basic structures and hashing, check password method

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module inventory-system
 
 go 1.17
 
+require github.com/labstack/echo/v4 v4.6.3
+
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -15,17 +17,16 @@ require (
 	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/labstack/echo/v4 v4.6.3 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20220208233918-bba287dce954 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/crypto v0.0.0-20220209195652-db638375bc3a // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
 	gorm.io/driver/postgres v1.2.3 // indirect
 	gorm.io/gorm v1.22.5 // indirect
 )
--- pkg/models/user.model.go
+++ pkg/models/user.model.go
@@ -2,26 +2,74 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
-type User struct {
-	ID       uuid.UUID `json:"id"`
-	Username string
-	Password string
-	Email    string
-	Role     string
+type (
+	User struct {
+		ID        uuid.UUID `json:"id"`
+		Username  string    `json:"username"`
+		Password  string    `json:"password"`
+		Email     string    `json:"email"`
+		Role      string    `json:"role"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+
+	CreateUserDto struct {
+		Username string
+		Password string
+		Email    string
+	}
+
+	UpdateUserDto struct {
+		Username string
+		Password string
+		Email    string
+		Role     string
+	}
+
+	UserResponse struct {
+		ID       uuid.UUID `json:"id"`
+		Username string    `json:"username"`
+		Email    string    `json:"email"`
+		Role     string    `json:"role"`
+	}
+)
+
+type (
+	Response struct {
+		Message string
+		Success bool
+		Data    interface{}
+	}
+)
+
+func (*User) CreateUser(payload *CreateUserDto) User {
+	return User{
+		ID:        uuid.New(),
+		Username:  payload.Username,
+		Password:  payload.Password,
+		Email:     payload.Email,
+		Role:      "Normal",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
 }
 
-type CreateUserDto struct {
-	Username string
-	Password string
-	Email    string
-	Role     string
+func (payload *User) CheckPassword(given_password string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(payload.Password), []byte(given_password)); err != nil {
+		return false
+	}
+	return true
 }
 
-type UpdateUserDto struct {
-	Username string
-	Password string
-	Email    string
-	Role     string
+func (payload *User) HashPassword() {
+	hashedpassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		panic(err)
+	}
+	*&payload.Password = string(hashedpassword)
 }
