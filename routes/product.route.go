292643b47b24a401269c1b825fb02ec81acac286
mystique09 <knightkrypto7@gmail.com>
2022-03-02 00:09:49 +0800

    add CRUD routes for products

--- routes/product.route.go
+++ routes/product.route.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"inventory-system/handlers"
+	"inventory-system/models"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -18,21 +21,71 @@ func (rt *Product) InitDb(conn *gorm.DB) {
 }
 
 func (rt *Product) GetAll(c echo.Context) error {
-	return c.JSON(http.StatusOK, "GetAll product.")
+	var products []models.Product = handlers.GetProducts(rt.Conn)
+
+	return c.JSON(http.StatusOK, products)
 }
 
 func (rt *Product) CreateOne(c echo.Context) error {
-	return c.JSON(http.StatusOK, "CreateOne product.")
+
+	payload := new(models.CreateProductDto)
+
+	if err := (&echo.DefaultBinder{}).BindBody(c, &payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	if payload.Owner == uuid.Nil {
+		return c.JSON(http.StatusBadRequest, "Missing owner field.")
+	}
+
+	if err := handlers.CreateProduct(rt.Conn, *payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusOK, "One product created.")
 }
 
 func (rt *Product) GetOne(c echo.Context) error {
-	return c.JSON(http.StatusOK, "GetOne product.")
+	uid, uid_error := uuid.Parse(c.Param("id"))
+
+	if uid_error != nil {
+		return c.JSON(http.StatusBadRequest, uid_error)
+	}
+
+	var product models.Product = handlers.GetProduct(rt.Conn, uid)
+	return c.JSON(http.StatusOK, product)
 }
 
 func (rt *Product) UpdateOne(c echo.Context) error {
-	return c.JSON(http.StatusOK, "UpdateOne product.")
+	uid, uid_err := uuid.Parse(c.Param("id"))
+	payload := new(models.UpdateProductDto)
+
+	if uid_err != nil {
+		return c.JSON(http.StatusBadRequest, uid_err)
+	}
+
+	if err := (&echo.DefaultBinder{}).BindBody(c, &payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	if err := handlers.UpdateProduct(rt.Conn, uid, *payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusOK, "Updated one product.")
 }
 
 func (rt *Product) DeleteOne(c echo.Context) error {
-	return c.JSON(http.StatusOK, "DeleteOne product.")
+
+	uid, uid_err := uuid.Parse(c.Param("id"))
+
+	if uid_err != nil {
+		return c.JSON(http.StatusBadRequest, uid_err)
+	}
+
+	if err := handlers.DeleteProduct(rt.Conn, uid); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusOK, "Deleted one product.")
 }
